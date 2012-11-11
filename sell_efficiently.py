58d095e30a7d51e299687e5ffcf1a6196bc7b502
Andy Sturges <arsturges@lbl.gov>
2012-11-11 12:50:49 -0800

    added new button

--- sell_efficiently.py
+++ sell_efficiently.py
@@ -47,60 +47,31 @@ def view_results():
 
 @app.route('/choose_products', methods=['GET','POST'])
 def choose_products():
-    if request.method == 'GET':
-        results_visibility = 'none'
-        db = connect_db()
+      results_visibility = 'none'
+      db = connect_db()
 
-        brands = db.execute('select distinct(brandName) from products order by type asc')
-        list_of_brands = []
-        for row in brands.fetchall():
-            list_of_brands.append(row)
+      brands = db.execute('select distinct(brandName) from products order by type asc')
+      list_of_brands = []
+      for row in brands.fetchall():
+          list_of_brands.append(row)
 
-        types = db.execute('select distinct(type) from products order by type asc')
-        list_of_types = []
-        for row in types.fetchall():
-            list_of_types.append(row)
+      types = db.execute('select distinct(type) from products order by type asc')
+      list_of_types = []
+      for row in types.fetchall():
+          list_of_types.append(row)
 
-        sizes = db.execute('select distinct(sizeSort) from products order by sizeSort desc')
-        list_of_sizes = []
-        for row in sizes.fetchall():
-            list_of_sizes.append(row)
+      sizes = db.execute('select distinct(sizeSort) from products order by sizeSort desc')
+      list_of_sizes = []
+      for row in sizes.fetchall():
+          list_of_sizes.append(row)
 
-        return render_template('choose_products.html', title = "Choose Products", 
-          brands=list_of_brands,
-          types = list_of_types,
-          sizes = list_of_sizes,
-          results_visibility = results_visibility,
-          results1=None,
-          results2=None) 
-    else:
-        results_visibility = 'block'
-        pic_urls = {}
-        pic_urls['Small'] = 'http://images.lowes.com/product/converted/036725/036725568419.jpg'
-        pic_urls['Medium'] = 'http://images.lowes.com/product/converted/883049/883049181479.jpg'
-        pic_urls['Large'] = 'http://images.lowes.com/product/converted/012505/012505698699.jpg'
-        pic_url_1 = pic_urls[request.form['size1']]
-        pic_url_2 = pic_urls[request.form['size2']]
-        product1_sql = "select * from products where modelNumber = 'KDDC24CVS'"
-        product2_sql = "select * from products where modelNumber = 'RF263AEWP'"
-
-        db = connect_db()
-        fridge1 = db.execute(product1_sql)
-        fridge2 = db.execute(product2_sql)
-        fridge1_attributes = []
-        for row in fridge1.fetchall():
-            fridge1_attributes.append(row)
-        fridge2_attributes = []
-        for row in fridge2.fetchall():
-            fridge2_attributes.append(row)
-            
-        return render_template('choose_products.html', 
-          title="Product Comparison Results", 
-          pic_url_1=pic_url_1,
-          pic_url_2=pic_url_2,
-          results_visibility=results_visibility,
-          results1=fridge1_attributes,
-          results2=fridge2_attributes)
+      return render_template('choose_products.html', title = "Choose Products", 
+        brands=list_of_brands,
+        types = list_of_types,
+        sizes = list_of_sizes,
+        results_visibility = results_visibility,
+        results1=None,
+        results2=None) 
 
 @app.route('/help')
 def help():
